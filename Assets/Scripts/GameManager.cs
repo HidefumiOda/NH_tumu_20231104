b1aa121191a28556c52dbae8adc11e528a6a3690
HidefumiOda <hidefumioda@gmail.com>
2023-11-11 10:53:09 +0900

    20231111

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    int nowBall;
+    int beforeBall;
+    int RANDOM_INDEX;
+    private IEnumerator DropBall()
+    {
+        // 生成するボールの種類を決める
+        RANDOM_INDEX = Random.Range(0, ballPrefabs.Length);
+
+        // 生成したボールの種類を保存する
+        beforeBall = nowBall;
+        nowBall = RANDOM_INDEX;
+
+        // もし前と同じボールを生成されるなら、生成されるボールの種類を変える
+        if (nowBall == beforeBall)
+        {
+            if(RANDOM_INDEX == 4)
+            {
+                RANDOM_INDEX = RANDOM_INDEX - 1;
+            }
+            else
+            {
+                RANDOM_INDEX = RANDOM_INDEX + 1;
+            }
+        }
+
+        // 生成するボールのX座標を決める
+        float RANDOM_X = Random.Range(-2.0f, 2.0f);
+        Vector3 BALL_INITIAL_POSITION = new Vector3(RANDOM_X, 7.0f, 0.0f);
+
+        // ボールを生成する
+        GameObject clonedBall = Instantiate(ballPrefabs[RANDOM_INDEX]);
+
+        // 生成したボールの位置を設定する
+        clonedBall.transform.position = BALL_INITIAL_POSITION;
+
+        // 0.5秒待つ
+        yield return new WaitForSeconds(0.5f);
+    }
     public GameObject[] ballPrefabs;
     void Start()
     {
         for(int i = 1; i <= 50; i++)
         {
-            int RANDOM_INDEX = Random.Range(0, ballPrefabs.Length);
-            GameObject clonedBall = Instantiate(ballPrefabs[RANDOM_INDEX]);
+            StartCoroutine("DropBall");
         }
     }
 
