ecd05740937cf9412589908a9010f1dc1b8c5674
HidefumiOda <hidefumioda@gmail.com>
2023-11-11 11:35:00 +0900

    20231111_2

---
+++ Assets/Scripts/CommonBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonBall : MonoBehaviour
+{
+    public bool isAdd;
+    public int kind0fld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,46 +9,50 @@ public class GameManager : MonoBehaviour
     int RANDOM_INDEX;
     private IEnumerator DropBall()
     {
-        // 生成するボールの種類を決める
-        RANDOM_INDEX = Random.Range(0, ballPrefabs.Length);
-
-        // 生成したボールの種類を保存する
-        beforeBall = nowBall;
-        nowBall = RANDOM_INDEX;
-
-        // もし前と同じボールを生成されるなら、生成されるボールの種類を変える
-        if (nowBall == beforeBall)
+        // 50回繰り返す
+        for (int i = 1; i <= 50; i++)
         {
-            if(RANDOM_INDEX == 4)
+            // 生成するボールの種類を決める
+            RANDOM_INDEX = Random.Range(0, ballPrefabs.Length);
+
+            // 生成したボールの種類を保存する (1)
+            beforeBall = nowBall;
+
+            // もし前と同じボールを生成されるなら
+            if (RANDOM_INDEX == beforeBall)
             {
-                RANDOM_INDEX = RANDOM_INDEX - 1;
+                // 生成されるボールの種類を変える
+                if (RANDOM_INDEX == 0)
+                {
+                    RANDOM_INDEX = RANDOM_INDEX + 1;
+                }
+                else
+                {
+                    RANDOM_INDEX = RANDOM_INDEX - 1;
+                }
             }
-            else
-            {
-                RANDOM_INDEX = RANDOM_INDEX + 1;
-            }
+
+            // 生成したボールの種類を保存する (2)
+            nowBall = RANDOM_INDEX;
+
+            // 生成するボールのX座標を決める
+            float RANDOM_X = Random.Range(-2.0f, 2.0f);
+            Vector3 BALL_INITIAL_POSITION = new Vector3(RANDOM_X, 7.0f, 0.0f);
+
+            // ボールを生成する
+            GameObject clonedBall = Instantiate(ballPrefabs[RANDOM_INDEX]);
+
+            // 生成したボールの位置を設定する
+            clonedBall.transform.position = BALL_INITIAL_POSITION;
+
+            // 0.5秒待つ
+            yield return new WaitForSeconds(0.5f);
         }
-
-        // 生成するボールのX座標を決める
-        float RANDOM_X = Random.Range(-2.0f, 2.0f);
-        Vector3 BALL_INITIAL_POSITION = new Vector3(RANDOM_X, 7.0f, 0.0f);
-
-        // ボールを生成する
-        GameObject clonedBall = Instantiate(ballPrefabs[RANDOM_INDEX]);
-
-        // 生成したボールの位置を設定する
-        clonedBall.transform.position = BALL_INITIAL_POSITION;
-
-        // 0.5秒待つ
-        yield return new WaitForSeconds(0.5f);
     }
     public GameObject[] ballPrefabs;
     void Start()
     {
-        for(int i = 1; i <= 50; i++)
-        {
-            StartCoroutine("DropBall");
-        }
+        StartCoroutine("DropBall");
     }
 
     // Update is called once per frame
